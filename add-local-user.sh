3d80dc1c0c50fc7d2a29bedd76e0c7011d00771e
ngako <laurent.ngako@gmail.com>
2016-09-18 06:48:37 +0200

    Rename local user name evironment.

--- add-local-user.sh
+++ add-local-user.sh
@@ -4,7 +4,7 @@
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback to default user
 USER_ID=${LOCAL_USER_ID:-9001}
-USER_NAME=${USER_NAME:-dev}
+USER_NAME=${LOCAL_USER_NAME:-dev}
 echo "create user $USER_NAME with UID $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m $USER_NAME
 # Change owner of dev folder
